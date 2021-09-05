02f7ed38f7a1bf3284086e5ee3d76b9a9095a277
Valentin Vasilev <59262958+vasilev02@users.noreply.github.com>
2021-09-05 14:09:42 +0300

    add not found page

--- routes.js
+++ routes.js
@@ -8,4 +8,8 @@ const router = Router();
 router.use(productController);
 router.use("/about", aboutController);
 
+router.get("*", (req, res) => {
+    res.render("404", {layout: false});
+});
+
 module.exports = router;
