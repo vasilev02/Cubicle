73231b0e329effd95449451af31e3a75b668d4c6
Valentin Vasilev <59262958+vasilev02@users.noreply.github.com>
2021-09-05 13:49:13 +0300

    add routes

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const handlebars = require("express-handlebars")
 const config = require("./config/config");
+const routes = require("./routes")
 
 const app = express();
 
 const expressConfig = require("./config/express")
 expressConfig(app);
 
-
-app.get("/", (req, res) => {
-    res.render('home', {layout: false});
-});
+app.use(routes);
 
 
 app.listen(config.PORT, () => console.log(`Server is running on PORT ${config.PORT}...`));
