d55158fc1d4d4268097fd5037dcb1f5f85b87ffd
Valentin Vasilev <59262958+vasilev02@users.noreply.github.com>
2021-09-06 15:22:36 +0300

    add search

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,20 @@
 const { Router } = require("express");
-const productService = require("../services/productService")
+const productService = require("../services/productService");
 
 const router = Router();
 
 router.get("/", (req, res) => {
-
-  const products = productService.getAll();
+  const products = productService.getAll(req.query);
   res.render("home", { title: "Home", products });
-
 });
 
 router.get("/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
-router.post("/create", (req, res) => {
+
+router.post("/create", validateProduct, (req, res) => {
   let data = req.body;
-
   productService.create(data);
-
   res.redirect("/");
 });
 
@@ -25,5 +22,19 @@ router.get("/details/:productId", (req, res) => {
   const product = productService.getProductById(req.params.productId);
   res.render("details", { title: "Product details", product });
 });
+
+function validateProduct(req, res, next) {
+  let isValid = true;
+
+  if (req.body.name.trim().length < 3) {
+    isValid = false;
+  } else if (req.body.description.trim().length < 10) {
+    isValid = false;
+  }
+
+  if (isValid) {
+    next();
+  }
+}
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -2,10 +2,26 @@ const uniqid = require("uniqid");
 const Cube = require("../models/Cube");
 const fs = require('fs');
 const path = require('path');
-const productsData = require('../config/products.json')
+const productsData = require('../config/products.json');
+const { search } = require("../controllers/productController");
 
-function getAll(){
-    return productsData;
+function getAll(query){
+
+    let products = productsData;
+
+    if(query.search){
+        products = products.filter(p => p.name.toLowerCase().includes(query.search));
+    }
+
+    if(query.from){
+        products = products.filter(p => Number(p.level) >= query.from);
+    }
+
+    if(query.to){
+        products = products.filter(p => Number(p.level) <= query.to);
+    }
+
+    return products;
 }
 
 function getProductById(id){
